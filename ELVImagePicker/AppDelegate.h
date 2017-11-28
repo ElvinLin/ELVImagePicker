51458223caf8ffda28303a3b4371f0ff4d013dfe
Elvin <elvin.lin@asuscloud.com>
2017-11-28 18:02:08 +0800

    create new project

---
+++ ELVImagePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ELVImagePicker
+//
+//  Created by Elvin on 2017/11/28.
+//  Copyright © 2017年 Elvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ELVImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ELVImagePicker
+//
+//  Created by Elvin on 2017/11/28.
+//  Copyright © 2017年 Elvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
